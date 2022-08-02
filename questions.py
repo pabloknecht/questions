84382dcf6923d345b042a099883b5f6caee54dc0
pabloknecht <pabloknecht@gmail.com>
2022-08-02 19:20:34 +0200

    idf cmplete

--- questions.py
+++ questions.py
@@ -1,6 +1,7 @@
 import nltk
 import sys
 import os
+import math
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -18,7 +19,9 @@ def main():
         filename: tokenize(files[filename])
         for filename in files
     }
+
     file_idfs = compute_idfs(file_words)
+    print(file_idfs)
 
     # Prompt user for query
     query = set(tokenize(input("Query: ")))
@@ -56,7 +59,7 @@ def load_files(directory):
     for filename in os.listdir(directory):
 
         # Open the files to read
-        with open(os.path.join(directory, filename) , 'r') as f:
+        with open(os.path.join(directory, filename) , 'r', encoding="utf8") as f:
             contents[filename] = f.read()
             f.close()
 
@@ -71,7 +74,8 @@ def tokenize(document):
     Process document by coverting all words to lowercase, and removing any
     punctuation or English stopwords.
     """
-    raise NotImplementedError
+    stopwords = nltk.corpus.stopwords.words('english')
+    return [word for word in nltk.tokenize.word_tokenize(document.lower()) if word.isalpha() and word not in stopwords]
 
 
 def compute_idfs(documents):
@@ -82,7 +86,23 @@ def compute_idfs(documents):
     Any word that appears in at least one of the documents should be in the
     resulting dictionary.
     """
-    raise NotImplementedError
+    idf_dict = {}
+
+    for text in documents.values():
+        for word in text:
+            if word not in idf_dict:
+                idf_dict[word] = compute_word_idf(word, documents)
+
+    return idf_dict
+
+def compute_word_idf(word, documents):
+    """
+    Given a dictionary of `documents` that maps names of documents to a list
+    of words and a specific word, return the IDF value of the word.
+    """
+    total_documents = len(documents)
+    num_documents_with_word = sum([1 if word in document else 0 for document in documents.values()])
+    return math.log(total_documents / num_documents_with_word)
 
 
 def top_files(query, files, idfs, n):
